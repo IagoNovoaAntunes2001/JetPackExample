62f0f1fd290b2808741e2b01510fb8ce0ad2cdc3
Iago Novoa Antunes <inovoaan@everis.com>
2020-08-14 14:14:22 -0300

    All dependencies were implemented and structure base of the project with navigation has done.

---
+++ app/src/main/java/com/example/jetpackandroid/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.jetpackandroid.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jetpackandroid.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
